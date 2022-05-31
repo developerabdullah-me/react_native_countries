45b5e872458ae3416e316660f17c62fc17535fa4
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-05-31 23:59:39 +0600

    single country

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{ useState,useEffect } from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
 import Countries from './Component/Countries';
 
 export default function App() {
-  const [countries, setCountries]=useState([])
-  useEffect(() => {
-  fetch('https://restcountries.com/v3.1/all')
-  .then(res => res.json)
-  .then(data => setCountries(data))
 
-  },[])
   return (
     <View style={styles.container}>
       <Text>Abeta e valo hye ja</Text>
-      <scroll>
-        {
-          countries.map(country => <Countries country={country} key={country.id}></Countries>
-          )
-        }
-      </scroll>
+    <Countries></Countries>
       <StatusBar style="auto" />
     </View>
   );
--- Component/Countries.js
+++ Component/Countries.js
@@ -1,12 +1,29 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import React,{ useState,useEffect } from 'react';
+import Country from './Country'
 
 export default function Countries() {
+  const [countries, setCountries]=useState([])
+  useEffect(() => {
+  fetch('https://restcountries.com/v3.1/all')
+  .then(res => res.json)
+  .then(data => setCountries(data))
+
+  },[])
   return (
     <View>
       <Text> hello country's</Text>
       <Text> Dabdullah</Text>
       <Text> Abdullah@b69</Text>
+
+
+      <scroll>
+        {
+          countries.map(country => <Country country={country} ></Country>
+          )
+        }
+      </scroll>
     </View>
   )
 }
