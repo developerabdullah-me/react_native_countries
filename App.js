288f43cf066b48f64c635bd891a560a7b4728ecd
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-05-31 20:54:09 +0600

    test

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import React,{ useState,useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Countries from './Component/Countries';
 
 export default function App() {
+  const [countries, setCountries]=useState([])
+  useEffect(() => {
+  fetch('https://restcountries.com/v3.1/all')
+  .then(res => res.json)
+  .then(data => setCountries(data))
+
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Abeta e valo hye ja</Text>
+      <scroll>
+        {
+          countries.map(country => <Countries country={country} key={country.id}></Countries>
+          )
+        }
+      </scroll>
       <StatusBar style="auto" />
     </View>
   );
---
+++ Component/Countries.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Countries() {
+  return (
+    <View>
+      <Text> hello country's</Text>
+      <Text> Dabdullah</Text>
+      <Text> Abdullah@b69</Text>
+    </View>
+  )
+}
